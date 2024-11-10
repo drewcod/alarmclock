908272ce1b49a5e86f688add3465bdddb9c9e175
drewcod <159642860+drewcod@users.noreply.github.com>
2024-11-09 19:41:14 -0500

    alarms list sorted

--- alarm-clock/src/Alarm.js
+++ alarm-clock/src/Alarm.js
@@ -2,15 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 function Alarm() {
   const [alarms, setAlarms] = useState([]);
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }));
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentTime(new Date().toLocaleTimeString());
+      setCurrentTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }));
 
       alarms.forEach(alarm => {
-        if (alarm.time === currentTime) {
-          // Play alarm sound
+        const alarmTime = new Date(`1970-01-01T${alarm.time}`);
+        const currentHour = new Date().getHours();
+        const alarmHour = alarmTime.getHours();
+        const currentAMPM = currentTime.split(' ')[1];
+        const alarmAMPM = alarm.time.split(' ')[1];
+
+        if (alarmHour === currentHour && currentAMPM === alarmAMPM) {
           const audio = document.querySelector('audio');
           audio.play();
         }
@@ -33,18 +38,25 @@ function Alarm() {
     setAlarms(updatedAlarms);
   };
 
+  // Sort alarms by time, earliest first, considering 12-hour format
+  const sortedAlarms = alarms.sort((a, b) => {
+    const aTime = new Date(`1970-01-01T${a.time}`);
+    const bTime = new Date(`1970-01-01T${b.time}`);
+    return aTime - bTime;
+  });
+
   return (
     <div>
       <h1>Alarm Clock</h1>
       <input type="time" onChange={handleAlarmTimeChange} />
-      <ul>
-        {alarms.map(alarm => (
+      <ol>
+        {sortedAlarms.map(alarm => (
           <li key={alarm.id}>
             {alarm.time}
             <button onClick={() => handleDeleteAlarm(alarm.id)}>Delete</button>
           </li>
         ))}
-      </ul>
+      </ol>
       <audio autoPlay>
         <source src="alarm-sound.mp3" type="audio/mpeg" />
       </audio>
